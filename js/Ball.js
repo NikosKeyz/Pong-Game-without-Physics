792a7c9b19e6b52ac3dabc0b9f10465c4240d1eb
Nikos Katsos <nikoskeyz@gmail.com>
2016-05-18 11:03:56 +0300

    Collision detection updated

--- js/Ball.js
+++ js/Ball.js
@@ -23,7 +23,7 @@ function Ball() {
 		ctx.globalAlpha = 0.9;
 		ctx.drawImage( this.animation[this.animationCounter], this.x, this.y);
 		ctx.restore();
-		
+
 		if ( this.animationCounter < 3 )
 			this.animationCounter++;
 		else
@@ -76,67 +76,86 @@ function Ball() {
 	
 		switch( this.direction ) {
 			case UP_RIGHT:
-			
-				// if hit on right player
-				if ( (this.y + this.height/2) > game.rPlayer.y 
-				  && (this.y + this.height/2) < (game.rPlayer.y + game.rPlayer.height) 
-				  && (this.x + this.width ) > (canvas.width - 40) ) {
+				/* Hit TOP border */
+				if ( this.y < 0 ) {
+					this.direction = DOWN_RIGHT;
+				}
+				/* Hit Right-Player */
+				else if ( this.y > game.rPlayer.y && this.y < (game.rPlayer.y + game.rPlayer.height) // ball.y between lPlayer's height
+					  &&  (this.x + this.width) > game.rPlayer.x 
+					  && (this.x + this.width) < (game.rPlayer.x + game.rPlayer.width) ) // ball.x in front of lPlayer
+				{ 
 					this.direction = UP_LEFT;
 					this.hitSound.play();
-				} else if ( this.y < 0 ){ // hit top border
-					this.direction = DOWN_RIGHT;
-				} else if ( this.x > canvas.width - this.width/2 ) { // hit right border
-					this.scoreSound.play();
-					game.lPlayer.addScore();
-					game.init();
 				}
-				
-				break;
-			case UP_LEFT:
-			
-				// if hit on left player
-				if ( (this.y + this.height/2) > game.lPlayer.y 
-				  && (this.y + this.height/2) < (game.lPlayer.y + game.lPlayer.height) 
-				  && this.x < 40 ) {
-					this.direction = UP_RIGHT;
-					this.hitSound.play();
-				} else if ( this.y < 0 ) { // hit top border
-					this.direction = DOWN_LEFT;
-				} else if ( this.x + this.width/2 < 0 ) { // hit left border
-					this.scoreSound.play();
-					game.rPlayer.addScore();
-					game.init();
-				}
-				
-				break;
-			case DOWN_RIGHT:
-			
-				// if hit on right player
-				if ( (this.y + this.height/2) > game.rPlayer.y 
-				  && (this.y + this.height/2) < (game.rPlayer.y + game.rPlayer.height) 
-				  && (this.x + this.width ) > (canvas.width - 40) ) {
-					this.direction = DOWN_LEFT;
-					this.hitSound.play();
-				} else if ( this.y > canvas.height - this.height ) {// hit bottom border
-					this.direction = UP_RIGHT;
-				} else if ( this.x > canvas.width - this.width/2 ) { // hit right border
+				/* Hit Right border - Left Player scores  */
+				else if ( (this.x + this.width) > canvas.width ) 
+				{
 					this.scoreSound.play();
 					game.lPlayer.addScore();
 					game.init();
 				}
 
 				break;
+			case UP_LEFT:
+				/* Hit TOP border */
+				if ( this.y < 0 ) 
+				{
+					this.direction = DOWN_LEFT;
+				}
+				/* Hit Left-Player */
+				else if ( this.y > game.lPlayer.y && this.y < (game.lPlayer.y + game.lPlayer.height) // ball.y between lPlayer's height
+					  &&  this.x > game.lPlayer.x && this.x < (game.lPlayer.x + game.lPlayer.width) ) // ball.x in front of lPlayer
+				{ 
+					this.direction = UP_RIGHT;
+					this.hitSound.play();
+				}
+				/* Hit Left border - Right Player scores  */
+				else if ( this.x < 0 ) 
+				{
+					this.scoreSound.play();
+					game.rPlayer.addScore();
+					game.init();
+				}
+				break;
+			case DOWN_RIGHT:
+				/* Hit BOTTOM border */
+				if ( this.y + this.height > canvas.height ) {
+					this.direction = UP_RIGHT;
+				}
+				/* Hit Left-Player */
+				else if ( this.y > game.rPlayer.y && this.y < (game.rPlayer.y + game.rPlayer.height) // ball.y between lPlayer's height
+					  &&  (this.x + this.width) > game.rPlayer.x 
+					  && (this.x + this.width) < (game.rPlayer.x + game.rPlayer.width) ) // ball.x in front of lPlayer
+				{ 
+					this.direction = DOWN_LEFT;
+					this.hitSound.play();
+				}
+				/* Hit Right border - Left Player scores  */
+				else if ( (this.x + this.width) > canvas.width ) 
+				{
+					this.scoreSound.play();
+					game.lPlayer.addScore();
+					game.init();
+				}
+				break;
 			case DOWN_LEFT:
-			
-				// if hit on left player
-				if ( (this.y + this.height/2) > game.lPlayer.y 
-				  && (this.y + this.height/2) < (game.lPlayer.y + game.lPlayer.height) 
-				  && this.x < 40 ) {
+
+				/* Hit BOTTOM border */
+				if ( this.y + this.height > canvas.height ) 
+				{
+					this.direction = UP_LEFT;
+				}
+				/* Hit Left-Player */
+				else if ( this.y > game.lPlayer.y && this.y < (game.lPlayer.y + game.lPlayer.height) // ball.y between lPlayer's height
+					  &&  this.x > game.lPlayer.x && this.x < (game.lPlayer.x + game.lPlayer.width) ) // ball.x in front of lPlayer
+				{ 
 					this.direction = DOWN_RIGHT;
 					this.hitSound.play();
-				} else if ( this.y > canvas.height - this.height ) {// hit bottom border
-					this.direction = UP_LEFT;
-				} else if ( this.x + this.width/2 < 0 ) { // hit left border
+				}
+				/* Hit Left border - Right Player scores  */
+				else if ( this.x < 0 ) 
+				{
 					this.scoreSound.play();
 					game.rPlayer.addScore();
 					game.init();
